a6efff88a18b0fac0405f6dc6a6cbdfb18c51d66
OhIAmFine <845782625@qq.com>
2017-08-19 10:06:40 +0800

    [+]unique an array

---
+++ unique.js
@@ -0,0 +1,17 @@
+/**
+* unique an array
+**/
+let unique = function(arr) {  
+  let hashTable = {};
+  let data = [];
+  for(let i=0,l=arr.length;i<l;i++) {
+    if(!hashTable[arr[i]]) {
+      hashTable[arr[i]] = true;
+      data.push(arr[i]);
+    }
+  }
+  return data
+ 
+}
+ 
+module.exports = unique;
