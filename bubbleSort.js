cfb7d91592f5c59624ff1b2241d463a397e37999
OhIAmFine <845782625@qq.com>
2017-08-21 19:50:57 +0800

    [+]function bubbleSort(arr) {

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {  
+    for(let i = 0,l=arr.length;i<l-1;i++) {
+        for(let j = i+1;j<l;j++) {
+          if(arr[i]>arr[j]) {
+                let tem = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tem;
+            }
+        }
+    }
+    return arr;
+}
+module.exports = bubbleSort;
