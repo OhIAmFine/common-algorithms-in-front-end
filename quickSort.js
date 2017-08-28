9b676b0e083b2cd427a74dfaee3fdf59e0ddb161
OhIAmFine <845782625@qq.com>
2017-08-28 19:56:41 +0800

    [+]快速排序

--- quickSort.js
+++ quickSort.js
@@ -1,10 +1,13 @@
 function quickSort(arr) {
+
     if(arr.length <= 1) {
         return arr;
     }
+
     let leftArr = [];
     let rightArr = [];
     let q = arr[0];
+
     for(let i = 1,l=arr.length; i<l; i++) {
         if(arr[i]>q) {
             rightArr.push(arr[i]);
@@ -12,7 +15,7 @@ function quickSort(arr) {
             leftArr.push(arr[i]);
         }
     }
- 
+    
     return [].concat(quickSort(leftArr),[q],quickSort(rightArr));
 }
  
